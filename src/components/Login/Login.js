e4a863e62c3c5f6decb0c2effd9028b4c71e35b3
DuongPC <pduong244@gmail.com>
2023-08-22 22:50:01 +0700

    Form Login Responsive

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,18 +2,21 @@ import './Login.scss';
 
 function Login(props) {
     return (
-        <div className='login-container mt-3'>
-            <div className='container'>
-                <div className='row'>
-                    <div className='col-md-7 content-left'>
+        <div className='login-container py-3 d-flex justify-content-center align-items-center'>
+            <div className='container login-content'>
+                <div className='row align-items-center px-3 px-sm-0'>
+                    <div className='col-12 d-none col-sm-7 d-sm-block content-left'>
                         <div className='brand'>DuongPC</div>
                         <div className='details'>Learn everything....</div>
                     </div>
-                    <div className='col-md-5 content-right d-flex flex-column gap-2 py-3'>
+                    <div className='col-12 col-sm-5 content-right d-flex flex-column gap-2 py-3'>
+                        <div className='brand d-sm-none d-block'>DuongPC</div>
                         <input placeholder='Email address or phone number' className='form-control' type='text'></input>
                         <input placeholder='Password' className='form-control' type='password'></input>
                         <button className='btn btn-primary'>Log in</button>
-                        <div className='text-center'>Forgotten password?</div>
+                        <span className='text-center'>
+                            <a className='forgot-password' href='#'>Forgotten password?</a>
+                        </span>
                         <hr></hr>
                         <div className='text-center'><button class="btn btn-success">Create new account</button></div>
                     </div>
